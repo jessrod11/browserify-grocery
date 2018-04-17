ce9087a8938dcd65612664c960a5372deb774de2
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-04-16 19:11:48 -0500

    item names printing to the page

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,3 +1,7 @@
+const data = require('./data');
+const itemsDom = require('./itemsDom');
+
+const departmentDivContainerDiv = document.getElementById('departments');
 const deptCards = document.getElementsByClassName('department');
 
 const showDeptName = (e) => {
@@ -17,10 +21,14 @@ const hideDeptName = (e) => {
 const showItems = (e) => {
   const departmentId = e.target.parentNode.children[0].dataset.deptId;
   console.log('data-id', departmentId);
+  const selectedItems = data.getItemsByDept(departmentId);
+  console.log('selected items', selectedItems);
+  departmentDivContainerDiv.innerHTML = '';
+  itemsDom(selectedItems);
   // ToDo: filter items by deptId
 };
 
-const addDepEvents = () => {
+const addDeptEvents = () => {
   for (let i = 0; i < deptCards.length; i++) {
     deptCards[i].addEventListener('mouseenter', showDeptName);
     deptCards[i].addEventListener('mouseleave', hideDeptName);
@@ -29,5 +37,5 @@ const addDepEvents = () => {
 };
 
 module.exports = {
-  addDepEvents,
+  addDeptEvents,
 };
