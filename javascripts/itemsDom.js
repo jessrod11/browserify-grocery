524de2c4544fc0b176a96bffa8ef2fa53a2108c6
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-04-16 19:32:36 -0500

    added bootstrap panel to itemsDom

--- javascripts/itemsDom.js
+++ javascripts/itemsDom.js
@@ -3,7 +3,17 @@ const itemsOutputDiv = document.getElementById('items');
 const itemsDomString = (items) => {
   let domString = '';
   items.forEach((item) => {
-    domString += `<h3>${item.name}</h3>`;
+    domString += `<div class="col-sm-3 item">`;
+    domString +=   `<div class="panel panel-info">`;
+    domString +=    `<div class="panel-heading">`;
+    domString +=    `<h3 class="panel-title"><h3>${item.name}</h3></h3>`;
+    domString += `</div> `;
+    domString += `<div class="panel-body">`;
+    domString +=   `Panel content`;
+    domString +=    `</div>`;
+    domString +=   `</div>`;
+    domString += `</div>`;
+
   });
   return domString;
 };
